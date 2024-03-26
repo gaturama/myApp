2d0baf4d0820cde533c1ab0e42101a94609ac01c
gaturama <bielsobv@gmail.com>
2024-03-25 21:53:46 -0300

    first

---
+++ src/Screens/HomeScreen.jsx
@@ -0,0 +1,16 @@
+
+import { Text, View } from "react-native";
+import styles from './../config/styles';
+
+export default function HomeScreen() {
+    return (
+        <View styles={styles.container}> 
+            <Text>Página Inicial</Text>
+           
+            <Card>
+                
+            </Card>
+        </View>
+
+    )
+}
